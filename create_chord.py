673863192eb50cb65f56b48610845cfc11310918
Benjamin Verdier <bverdier@bu.edu>
2018-11-27 18:01:02 -0500

    Finalizing py2 to py3 port

--- create_chord.py
+++ create_chord.py
@@ -6,11 +6,11 @@ import random
 from chord import *
 
 nnodes = int(sys.argv[1])
-print "Creating chord network with : %s nodes" % nnodes
+print("Creating chord network with : %s nodes" % nnodes)
 
 # create ports
 ports_list = [random.randrange(10000, 60000) for x in range(nnodes)]
-print "Ports list : %s" % ports_list
+print("Ports list : %s" % ports_list)
 # create addresses
 address_list = map(lambda port: Address('127.0.0.1', port), ports_list)
 # keep unique ones
@@ -27,27 +27,27 @@ for i in range(0, len(address_list)):
 		remote = locals_list[random.randrange(len(locals_list))].address_
 		local = Local(address_list[i], remote)
 	local.start()
-	print "Created at %s"  % address_list[i]
+	print("Created at %s"  % address_list[i])
 	locals_list.append(local)
 	time.sleep(0.5)
 
-print "Done creating peers, our pid %s is" % os.getpid()
+print("Done creating peers, our pid %s is" % os.getpid())
 
 while 1:
-	command = raw_input("Command: ")
+	command = input("Command: ")
 	if command == "add_node":
 		while 1:
 			address = Address("127.0.0.1", random.randrange(10000, 60000))
 			if not address.__hash__() in hash_list:
 				ports_list.append(address.port)
-				print "New node at port %s" % address.port
+				print("New node at port %s" % address.port)
 				address_list.append(address)
-				locals_list.append(Local(address, locals_list[random.randrange(len(locals_list))].address))
+				locals_list.append(Local(address, locals_list[random.randrange(len(locals_list))].address_))
 				break
 	else:
 		address = address_list[random.randrange(len(address_list))]
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((address.ip, address.port))
-		s.sendall(command + "\r\n")
-		print "Response : '%s'" % s.recv(10000)
+		s.sendall((command + "\r\n").encode())
+		print("Response : '%s'" % s.recv(10000).decode())
 		s.close()
